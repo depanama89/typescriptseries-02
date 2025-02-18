754bb629472340bc5fbb8f754c9e9e9709612e60
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-02-18 13:53:09 +0100

    definition de la fonction numerique

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,17 @@
-console.log("test");
-console.log("test");
+/**
+ * Écrire une fonction qui prend un tableau de nombres et retourne le plus grand nombre
+ */
+function tab(tab: number[]): number {
+  if (tab.length === 0) {
+    throw new Error("le tableau est vide");
+  }
+  let max = tab[0];
+  for (let i = 0; i < tab.length; i++) {
+    if (tab[i] > max) {
+      max = tab[i];
+    }
+  }
+  return max;
+}
+const nombres = [30, 3, 20, 9, 200, 100];
+console.log(tab(nombres));
